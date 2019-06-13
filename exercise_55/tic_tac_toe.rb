fff44e45a21889d4221808819ef77255ad7ed67a
Jack <superjackpower85@gmail.com>
2019-06-13 17:08:34 +0800

    12June exes

--- exercise_55/tic_tac_toe.rb
+++ exercise_55/tic_tac_toe.rb
@@ -1,21 +1,21 @@
-class Board
+# class Board
 
-    def initialize
-    end
+#     def initialize
+#     end
 
-    def won?
-    end
+#     def won?
+#     end
 
-    def winner
-    end
+#     def winner
+#     end
 
-    def empty?(pos)
-    end
+#     def empty?(pos)
+#     end
 
-    def mark_place(pos, mark)
-    end
+#     def mark_place(pos, mark)
+#     end
 
-    #More methods needed... What are they?
-end
+#     #More methods needed... What are they?
+# end
 
 # More classes needed... What are they?
